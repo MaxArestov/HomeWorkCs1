da2844a655a32920c1808f3d8a0201102ebe8205
maxim arestov <aresmax1993@gmail.com>
2022-10-16 13:21:34 +0300

    Complete case 1

---
+++ Case1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Clear();
+Console.Write("Введите поочередно 2 числа, после каждого нажмите Enter: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (numberA > numberB) 
+{
+    max = numberA;
+}
+else 
+{
+     max = numberB;
+}
+Console.WriteLine(max);
